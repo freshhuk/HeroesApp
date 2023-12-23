f95fc3eb835ef0e50b31ea0e2ca64e7752b7af00
freshhuk <freshhuk13@gmail.com>
2023-12-24 00:27:39 +0200

    Login method is working

---
+++ src/main/java/com/heroes/heroesapp/Services/AutorizationService.java
@@ -0,0 +1,59 @@
+package com.heroes.heroesapp.Services;
+
+import com.heroes.heroesapp.Domain.Entity.User;
+import com.heroes.heroesapp.Domain.Models.LoginRequest;
+import com.heroes.heroesapp.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class AutorizationService
+{
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    @Autowired
+    public AutorizationService(UserRepository userRepository, PasswordEncoder passwordEncoder)
+    {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public String UserLogin(LoginRequest loginModel)
+    {
+        if(loginModel == null)
+        {
+            return "Not fond";
+        }
+        Optional<User> user = userRepository.GetUserByLogin(loginModel.getLogin());
+        // Пользователь не найден
+        if (user.isEmpty())
+            return "User not found";
+        // Пользователь найден, сделать его авторизованным
+        Authentication authentication = new UsernamePasswordAuthenticationToken
+                (
+                        user.get().getUsername(),
+                        loginModel.getPassword(),
+                        user.get().getAuthorities()
+                );
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        return "Done";
+    }
+
+    public String UserRegister(User user)
+    {
+        if(user != null)
+        {
+            user.setPassword(passwordEncoder.encode(user.getPassword()));
+            userRepository.Add(user);
+            return "Done";
+        }
+        return "Model is null";
+    }
+}
