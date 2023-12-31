a654c1484c1b42b7a9d88e49c22f1674b4cdb127
freshhuk <freshhuk13@gmail.com>
2023-12-31 23:46:47 +0200

    Last commit in 2023

--- src/test/java/com/heroes/heroesapp/Services/AutorizationServiceTest.java
+++ src/test/java/com/heroes/heroesapp/Services/AutorizationServiceTest.java
@@ -1,4 +1,46 @@
 package com.heroes.heroesapp.Services;
 
-public class AutorizationServiceTest {
+
+import com.heroes.heroesapp.Domain.Enum.Role;
+import com.heroes.heroesapp.Repository.UserRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import com.heroes.heroesapp.Domain.Models.LoginRequest;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import com.heroes.heroesapp.Domain.Entity.User;
+
+import java.util.Optional;
+
+public class AutorizationServiceTest
+{
+    @InjectMocks
+    AutorizationService authService;
+
+    @Mock
+    UserRepository user_repos;
+
+    @Test
+    void UserLoginTest()
+    {
+        LoginRequest model = new LoginRequest();
+        {
+            model.setLogin("test");
+            model.setPassword("test");
+        }
+        User user_model = new User();
+        {
+            user_model.setLogin("test");
+            user_model.setPassword("test");
+            user_model.setRole(Role.DEFAULT_USER);
+        }
+        Mockito.when(user_repos.GetUserByLogin("test")).thenReturn(Optional.of(user_model));
+
+        var result = authService.UserLogin(model);
+
+        String massage = "Done";
+        Assertions.assertEquals(result,massage);
+
+    }
 }
